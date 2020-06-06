348ce0c93ab1dbb63473acff69efe375a2ea2ba5
arianna choza <unachoza@gmail.com>
2020-06-06 14:30:20 -0400

    adding sass architecture

---
+++ src/Components/Header.component.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="Header__container">
+      <header className="Header__header">Header</header>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/Pages/Project.pages.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProjectPage = () => {
+  return (
+    <div className="ProjectPage__container">
+      <header className="ProjectPage__header">Project</header>
+    </div>
+  );
+};
+
+export default ProjectPage;
