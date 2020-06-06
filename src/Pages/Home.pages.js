ef47971b1337b40a53d44041eda1dd45a894ab85
arianna choza <unachoza@gmail.com>
2020-06-06 13:25:12 -0400

    Components and Pages

---
+++ src/Pages/Home.pages.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="home__container">
+      <header className="home__header">UI Portfolio Arianna Choza</header>
+    </div>
+  );
+};
+
+export default Home;
