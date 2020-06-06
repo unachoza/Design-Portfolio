ae8be85ec95b3f25856a39d72dec860bcd3649c6
arianna choza <unachoza@gmail.com>
2020-06-06 14:14:48 -0400

    adding sass architecture

--- src/Pages/Home.pages.js
+++ src/Pages/Home.pages.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from 'Components/Header.component'
 
 const Home = () => {
   return (
     <div className="home__container">
       <header className="home__header">UI Portfolio Arianna Choza</header>
+      <Header/>
     </div>
   );
 };
